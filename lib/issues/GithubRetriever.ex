4c4f6f905113b952263ba02286eaae06c283d2c5
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-08-05 23:55:38 -0500

    Get url and handle reponse

--- lib/issues/GithubRetriever.ex
+++ lib/issues/GithubRetriever.ex
@@ -1,5 +1,13 @@
 defmodule GithubRetriever do
+  @user_agent [{"User-agent", "Elixir hectorivanpatriciomoreno@gmail.com"}]
   def get(user, project) do
-
+    url(user, project)
+      |> HTTPoison.get(@user_agent)
+      |> handle_response
   end
+  def url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
+  def handle_response(%{status_code: 200, body: body}), do { :ok, body }
+  def handle_response(%{status_code: ___, body: body}), do { :error, body }
 end
