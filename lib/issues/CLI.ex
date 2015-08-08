3f27216e0b8a65d7639e9ef80513c6dbc67252ad
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2015-08-07 20:51:36 -0500

    Creating table correctly, at last

--- lib/issues/CLI.ex
+++ lib/issues/CLI.ex
@@ -81,15 +81,15 @@ defmodule Issues.CLI do
     max = Enum.into(max, HashDict.new)
     s = ''
     Enum.each headers, fn h ->
-      IO.write (String.ljust h[:label], max[h[:name]]) <> "| "
+      IO.write " " <> (String.ljust h[:label], max[h[:name]]) <> "|"
       #s = s <> (String.duplicate "-", max[h[:name]]) <> "+"
     end
     IO.puts ""
-    Enum.each headers, fn h -> IO.write (String.duplicate "-", max[h[:name]]) <> "+" end
+    Enum.each headers, fn h -> IO.write (String.duplicate "-", max[h[:name]] + 1 ) <> "+" end
     IO.puts ""
     Enum.each list, fn element ->
       Enum.each headers, fn header ->
-        IO.write (String.ljust header[:trans].(element[header[:name]]), max[header[:name]]) <> "| "
+        IO.write " " <> (String.ljust header[:trans].(element[header[:name]]), max[header[:name]]) <> "|"
       end
       IO.puts ""
     end
